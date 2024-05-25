4cbd075f4d22dd9167a4176cac04d2f000fffbf6
Cristian Leal <cristianmape@live.com>
2024-05-24 22:48:33 -0500

    Creacion del backend conpleto y modificaciones al query de la base de datos

--- server/index.js
+++ server/index.js
@@ -1,10 +1,85 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const db = pass; 
+const db = mysql.createConnection({
+  host:"localhost",
+  port: 3306,
+  user:"root",
+  password:"admin",
+  database:"biblioteca"
+});
+
+// Ver todos los prestamos
+app.get("/prestamos", (req, res) => {
+  const sqlSelect = "SELECT * FROM VistaPrestamos";
+  db.query(sqlSelect, (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+  });
+});
+
+// Ver el historial de los prestamos
+app.get("/historial", (req, res) => {
+  const sqlSelect = "SELECT * FROM VistaHistorial";
+  db.query(sqlSelect, (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+  });
+});
+
+// Registrar libro
+app.post("/addLibro", (req, res) => {
+  const { tituloLibro, editorial, autor, fecha, campoEstudio, cantidad } = req.body;
+  const sqlInsert = "CALL InsertarLibro(?,?,?,?,?,?);";
+  db.query(sqlInsert, [tituloLibro, editorial, autor, fecha, campoEstudio, cantidad], (err, result) => {
+      if (err) console.log(err);
+      else res.send("Libro registrado");
+  });
+});
+
+// Registrar prestamo
+app.post("/addPrestamo", (req, res) => {
+  const { cedula, nombreCompleto, carrera, correo, numero, tituloLibro } = req.body;
+  const sqlInsert = "CALL RegistrarPrestamo(?,?,?,?,?,?);";
+  db.query(sqlInsert, [cedula, nombreCompleto, carrera, correo, numero, tituloLibro], (err, result) => {
+      if (err) console.log(err);
+      else res.send("Prestamo registrado");
+  });
+});
+
+// Actualizar estado del prestamo (devolucion del libro)
+app.post("/actualizarEstado", (req, res) => {
+  const { idPrestamo } = req.body; // el id del registro del prestamo
+  const sqlInsert = "CALL ActualizarEstado(?);";
+  db.query(sqlInsert, [idPrestamo], (err, result) => {
+      if (err) console.log(err);
+      else res.send("estado del prestamo actualizado");
+  });
+});
+
+// Validar usuario (inicio de sesion)
+app.post("/validarUsuario", (req, res) => {
+  const { nombreUsuario, contrasena } = req.body; 
+  const sqlInsert = "CALL ValidateUser(?,?);";
+  db.query(sqlInsert, [nombreUsuario, contrasena], (err, result) => {
+      if (err) {
+          console.log(err);
+      } else {
+          // Obtener el resultado del procedimiento almacenado
+          const resultado = result[0][0].Resultado;
+          if (resultado) {
+              res.send("usuario validado");
+          } else {
+              res.send("usuario no validado");
+          }
+      }
+  });
+});
 
 app.listen(3001,()=>{
   console.log("Corriendo en el puerto 3001")
